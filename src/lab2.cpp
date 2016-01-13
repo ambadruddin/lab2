755368fff75751f76a40df1138010bb642cde8b0
Amaan <amaanbadruddin2018@u.northwestern.edu>
2016-01-13 14:48:45 -0600

    Commit

--- src/lab2.cpp
+++ src/lab2.cpp
@@ -4,6 +4,7 @@
 // Lab 2 programs
 //
 
+/*
 int illegalStatements() {
     /* Start here! */
     //int double = 0;
@@ -13,12 +14,55 @@ int illegalStatements() {
     //int pi = 3.14;
     //short num = 1e9;
     //string mystring = 5;
-    return 0;
-}
+    //return 0;
+//}
 
+/*
 int main() {
     illegalStatements();
     cout << "Start with Exercise 1 (legal/illegal statements)" << endl;
     cout << "Then work on the exercises and code them up." << endl;
-}
+} */
 
+
+using namespace std;
+
+//
+//void odd_or_even(int num)
+//{
+//    if(num%2 == 0)
+//        cout << num << " is even\n";
+//    else
+//        cout << num << " is odd\n";
+//}
+//
+//int main() {
+//    odd_or_even(5);
+//    odd_or_even(8);
+//    return 0;
+//}
+
+//int main()
+//{
+//    cout << "Enter values\n";
+//    double x1, x2;
+//    char opr;
+//    cin >> opr >> x1 >> x2;
+//    if(opr == '+')
+//        cout << x1 + x2;
+//    if(opr == '-')
+//        cout << x1 - x2;
+//    if(opr == '/')
+//        cout << x1/x2;
+//    if(opr == '*')
+//        cout << x1 * x2;
+//
+//}
+
+enum friend_gender(m, f)
+
+int main()
+{
+    string friend_gender = '';
+
+}
